1611af34937e03dc692580961b5b0a80e22d6133
Peter Imoesi <peterimoesi@yahoo.com>
2018-10-13 13:31:19 +0300

    Updates

---
+++ precache-manifest.3a716e12f0e1b32416aac072f66d5a12.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/kodit-challenge/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "085c78ab1fee6355a7dd",
+    "url": "/kodit-challenge/static/css/main.defbb05a.chunk.css"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/kodit-challenge/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "16e5f75a27d0a9fe07cb",
+    "url": "/kodit-challenge/static/js/1.16e5f75a.chunk.js"
+  },
+  {
+    "revision": "0ceb6f5a54ddeed342d6",
+    "url": "/kodit-challenge/static/js/runtime~main.0ceb6f5a.js"
+  },
+  {
+    "revision": "ace129c87bfa086b073d767f1fdec0d5",
+    "url": "/kodit-challenge/static/media/house1.ace129c8.jpg"
+  },
+  {
+    "revision": "3ed274f670302dd278dea58869c4778f",
+    "url": "/kodit-challenge/static/media/house2.3ed274f6.jpg"
+  },
+  {
+    "revision": "61fbb9cfe5784efc6dee19fd386e2ea8",
+    "url": "/kodit-challenge/static/media/house3.61fbb9cf.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/kodit-challenge/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/kodit-challenge/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kodit-challenge/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kodit-challenge/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/kodit-challenge/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/kodit-challenge/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/kodit-challenge/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/kodit-challenge/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/kodit-challenge/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/kodit-challenge/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "085c78ab1fee6355a7dd",
+    "url": "/kodit-challenge/static/js/main.085c78ab.chunk.js"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/kodit-challenge/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/kodit-challenge/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/kodit-challenge/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/kodit-challenge/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/kodit-challenge/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/kodit-challenge/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/kodit-challenge/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/kodit-challenge/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/kodit-challenge/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/kodit-challenge/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/kodit-challenge/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/kodit-challenge/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/kodit-challenge/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/kodit-challenge/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/kodit-challenge/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "16e5f75a27d0a9fe07cb",
+    "url": "/kodit-challenge/static/css/1.48c75e55.chunk.css"
+  },
+  {
+    "revision": "299050c7ccf7813536372c94daa55ef9",
+    "url": "/kodit-challenge/index.html"
+  }
+];
